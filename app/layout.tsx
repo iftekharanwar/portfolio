c7bbb402d41f4269a138f2c46043e1d64adf0540
Iftekhar Anwar <116110091+iftekharanwar@users.noreply.github.com>
2025-12-28 04:36:52 +0100

    feat: add dns-prefetch links

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,6 +93,8 @@ export default function RootLayout({
         <meta name="theme-color" content="#5A3C0B" />
         <meta name="color-scheme" content="light" />
         <meta name="format-detection" content="telephone=no" />
+        <link rel="preconnect" href="https://formspree.io" />
+        <link rel="dns-prefetch" href="https://formspree.io" />
         <link rel="icon" type="image/png" sizes="96x96" href="/icon.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" />
